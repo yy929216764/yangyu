711cd58e9dae89a2f8dd9712960c2e5ddce51bed
杨柳依依 <929216764@qq.com>
2023-12-14 12:16:29 +0800

    init

---
+++ src/main/java/com/yangyu/demo/te1.java
@@ -0,0 +1,14 @@
+package com.yangyu.demo;
+
+/**
+ * @program: yangyu
+ * @ClassName te1
+ * @description:
+ * @author: yangyu
+ * @create: 2023-12-14 12:14
+ */
+public class te1 {
+    //yy1新增一个
+
+    //保存了吗
+}
